659d500f0515d794dac9f67c8827982df7cbc67d
schrödinger <118077794+schrodinger0000@users.noreply.github.com>
2022-12-05 18:14:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import socket
+
+sc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ip = input("Insert IP: ")
+edp = int(input("Insert the least port value: "))
+ebp = int(input("Insert the greatest port value: "))
+
+
+for port in range(edp,ebp+1):
+    try:
+        sc.connect((ip.port))
+        print("{}. port is open")
+    except:
+        pass
